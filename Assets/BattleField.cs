19881fc8f092c1f8156056b6d476caef7f439f6e
Wolf4D <Wolf4D@list.ru>
2019-10-04 16:11:14 +0300

    Динамически генерируется поле боя

---
+++ Assets/BattleField.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleField : MonoBehaviour
+{
+    public int x = 1;
+    public int y = 1;
+    public GameObject CellSample;
+    public GameObject[,] Map;
+    public float cellSideSize = 4.77939f * 1.2885f;
+
+    public Vector3 CalcXYZfromCoords(int x, int y)
+    {
+        return new Vector3(x * 2.0f * cellSideSize, 0, y * 2.0f * cellSideSize);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Map = new GameObject[x, y];
+        for (int j = 0; j < y; j++)
+            for (int i = 0; i < x; i++)
+        {
+            Map[i,j] = Instantiate(CellSample, this.transform);
+                Map[i, j].transform.localPosition = CalcXYZfromCoords(i, j);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ShipProperties.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipProperties : MonoBehaviour
+{
+    public enum BattleSides
+    {
+        Neutral = 0,
+        Earth = 1,
+        Asura = 2,
+        Heretic = 3
+    };
+
+    public BattleSides side = BattleSides.Earth;
+
+    public int HP;
+    public int attackDistance;
+    public int attackPower;
+    public int attackCooldown;
+    public int attackCooldownLeft = 0;
+    public int moveSpeed;
+    public bool hasMoved = false;
+
+    public int specialPropertyCooldownLeft = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {                 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
