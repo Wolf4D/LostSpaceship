2847a3e8638c079ca15d2624753fe4b542660714
Wolf4D <Wolf4D@list.ru>
2019-10-04 19:01:07 +0300

    Выделение

--- Assets/BattleField.cs
+++ Assets/BattleField.cs
@@ -8,6 +8,8 @@ public class BattleField : MonoBehaviour
     public int y = 1;
     public GameObject CellSample;
     public GameObject[,] Map;
+    public GameObject[,] Objects;
+    //public GameObject[] ObjectsToSnap; // объекты, которые должны быть подтянуты на карту
     public float cellSideSize = 4.77939f * 1.2885f;
 
     public Vector3 CalcXYZfromCoords(int x, int y)
@@ -15,18 +17,37 @@ public class BattleField : MonoBehaviour
         return new Vector3(x * 2.0f * cellSideSize, 0, y * 2.0f * cellSideSize);
     }
 
+    public Vector2 CalcCoordsFromXYZ(Vector3 xyz)
+    {
+        return new Vector2(Mathf.RoundToInt(xyz.x / (2.0f * cellSideSize)), Mathf.RoundToInt(xyz.z / (2.0f * cellSideSize)));
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
+        Objects = new GameObject[x, y];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            GameObject obj = transform.GetChild(i).gameObject;
+            Vector2 coord = CalcCoordsFromXYZ(obj.transform.localPosition);
+            //Debug.Log("" + obj.name + ":" + coord.x + "," + coord.y);
+            Objects[(int)(coord.x), (int)(coord.y)] = obj;
+        }
+
         Map = new GameObject[x, y];
         for (int j = 0; j < y; j++)
             for (int i = 0; i < x; i++)
         {
             Map[i,j] = Instantiate(CellSample, this.transform);
                 Map[i, j].transform.localPosition = CalcXYZfromCoords(i, j);
+                Map[i, j].name = i + ", " + j;
         }
-        
+    }
+
+    public GameObject GetObjectAtCoords(Vector2 coord)
+    {
+        return Objects[(int)(coord.x), (int)(coord.y)];
     }
 
     // Update is called once per frame
---
+++ Assets/MouseControl.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour
+{
+
+    public BattleField[] BattleFields;
+    public BattleField CurrentBattleField = null;
+    public GameObject MouseTrackBorder;
+    public GameObject MouseSelectionBorder;
+    public GameObject cellUnderMouse;
+    public GameObject SelectedObject;
+    // private Camera cameraUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BattleFields = FindObjectsOfType<BattleField>();
+        //cameraUI = GetComponent<Camera>();
+        MouseTrackBorder = Instantiate(MouseTrackBorder);
+        MouseSelectionBorder = Instantiate(MouseSelectionBorder);
+        MouseSelectionBorder.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProbeBattleFieldsForHover();
+
+        if (Input.GetMouseButton(0))
+            TrySelect();
+    }
+
+    bool TrySelect()
+    {
+        if ((CurrentBattleField==null) || (cellUnderMouse == null))
+            return false;
+
+        Vector2 coords = CurrentBattleField.CalcCoordsFromXYZ(cellUnderMouse.transform.localPosition);
+        GameObject toSelect = CurrentBattleField.GetObjectAtCoords(coords);
+
+        //Debug.Log(toSelect);
+        if (toSelect != SelectedObject)
+        {
+            ProceedSelection(toSelect);
+            return true;
+        }
+
+        return false;
+    }
+
+    void ProceedSelection(GameObject newObj)
+    {
+        MouseSelectionBorder.SetActive(false);
+        SelectedObject = newObj;
+
+        if (SelectedObject != null)
+        {
+            MouseSelectionBorder.SetActive(true);
+            MouseSelectionBorder.transform.position = SelectedObject.transform.position;
+        }
+        
+
+
+    }
+
+    void ProbeBattleFieldsForHover()
+    {
+        MouseTrackBorder.SetActive(false);
+        cellUnderMouse = GetCellUnderMouse();
+        if (cellUnderMouse!=null)
+        { 
+        MouseTrackBorder.transform.position = cellUnderMouse.transform.position;
+        MouseTrackBorder.SetActive(true);
+            // Пока так
+            CurrentBattleField = cellUnderMouse.transform.parent.GetComponent<BattleField>();
+        }
+    }
+
+    GameObject GetCellUnderMouse()
+    {
+
+        // Проверяем, не попали ли в UI?
+
+        Vector3 mouseOptPos = Input.mousePosition;
+        mouseOptPos.y += 30;
+        Ray ray = Camera.main.ScreenPointToRay(mouseOptPos);
+        RaycastHit hit;
+        var layerMask = 1 << LayerMask.NameToLayer("BattleGrid");
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask, QueryTriggerInteraction.UseGlobal))
+            {
+            //Debug.Log(hit.collider.gameObject.name);
+            Debug.DrawLine(ray.origin, hit.point);
+
+            for (int i = 0; i < BattleFields.Length; i++)
+            {
+                foreach(GameObject obj in BattleFields[i].Map)
+                if (obj == hit.collider.gameObject)
+                        return obj;
+            }
+        }
+
+        return null;
+        //  CreatureSelected.Invoke(selected);
+
+    }
+
+}
