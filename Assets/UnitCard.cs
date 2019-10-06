a62b3fe6ae6dc371ff2c018f809bbecf23602de8
Wolf4D <Wolf4D@list.ru>
2019-10-06 23:44:26 +0300

    Перенос и добавление ресурсов

---
+++ Assets/UnitCard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitCard : MonoBehaviour
+{
+    public GameObject ShipToBuy;
+    public int Cost;
+    public Sprite UnitSprite;
+    public Text UnitName;
+    public Text UnitCost;
+    public Image Picture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShipProperties ship = ShipToBuy.GetComponent<ShipProperties>();
+        //Text UnitName = GetComponentsInChildren<Text>()[1];
+        //Text UnitCost = GetComponentsInChildren<Text>()[2];
+        //Image Picture = GetComponentInChildren<Image>();
+
+        Picture.sprite = UnitSprite;
+        UnitCost.text = Cost + "$";
+        UnitName.text = ship.unitName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
