5d0417b950d7cd8c46cd0b1757e5c1d5918d413c
Wolf4D <Wolf4D@list.ru>
2019-10-07 22:50:18 +0300

    Интро, блин

---
+++ Assets/EnableByTimer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableByTimer : MonoBehaviour
+{
+    public GameObject[] toEnable;
+    public float[] timeToHold;
+    public int currentObject = 0;
+    public float timeLeft = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if ((Input.anyKeyDown) || (timeLeft<=0))
+        {
+          if (toEnable[currentObject]!=null)
+            toEnable[currentObject].SetActive(false);
+            currentObject++;
+            toEnable[currentObject].SetActive(true);
+            timeLeft = timeToHold[currentObject];
+
+        }
+    }
+
+    
+
+}
---
+++ Assets/MoveZ.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveZ : MonoBehaviour
+{
+    public float end = -152;
+    public float speed = 0.5f;
+    public bool opposite = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!opposite)
+        {
+            if (transform.position.z > end)
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed * Time.deltaTime);
+        }
+        else
+        if (transform.position.z < end)
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed * Time.deltaTime);
+      
+    }
+}
