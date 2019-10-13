f3bbbb5f3f2ef6bfb8701b9ded052706bff8dfbd
Wolf4D <Wolf4D@list.ru>
2019-10-13 04:34:39 +0300

    Второй уровень круто доделан

--- Assets/ActivateToo.cs
+++ Assets/ActivateToo.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class ActivateToo : MonoBehaviour
 {
     public GameObject activateToo;
+    public bool doItOnStart = true;
     // Start is called before the first frame update
     void Start()
     {
-        activateToo.SetActive(true);
+        if (doItOnStart)
+            activateToo.SetActive(true);
+    }
+
+    private void OnEnable()
+    {
+        if (!doItOnStart)
+            activateToo.SetActive(true);
     }
 
     // Update is called once per frame
---
+++ Assets/SecondLevelScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondLevelScript : MonoBehaviour
+{
+    SidesStats stats;
+    UnitSpawner spawner;
+    TurnSystem turnSystem;
+    public int phase = 1;
+    public GameObject phase1;
+    public GameObject phase2;
+    public GameObject phase3;
+    public GameObject phase4;
+    public GameObject phase5;
+
+    public GameObject playersBase;
+    public GameObject disruptorShip;
+
+    public GameObject lose;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        turnSystem = FindObjectOfType<TurnSystem>();
+        stats = FindObjectOfType<SidesStats>();
+        spawner = FindObjectOfType<UnitSpawner>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playersBase == null)
+        {
+            lose.SetActive(true);
+            phase = -1;
+        }
+
+        if (disruptorShip == null)
+        {
+            lose.SetActive(true);
+            phase = -1;
+        }
+    }
+}
